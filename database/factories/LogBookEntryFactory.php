117d01efdc2fdc2767ef5e71f2493d445c283997
big-dawg-bit <raspearnaud@gmail.com>
2026-06-05 12:55:16 +0200

    add core data model migrations and models

---
+++ database/factories/LogBookEntryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LogBookEntry;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<LogBookEntry>
+ */
+class LogBookEntryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
