be35b42fc04afdb30a3f42e699ef3a4ea84c40db
FuturesProspect223 <77144077+PhilippeAI2025@users.noreply.github.com>
2026-06-13 22:25:21 +0200

    Feat:render fucnie in ReviewQueue.php

--- app/Livewire/Applications/ReviewQueue.php
+++ app/Livewire/Applications/ReviewQueue.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Livewire\Applications;
+
+use App\Models\StageApplication;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+#[Layout('layouts.portal')]
+class ReviewQueue extends Component
+{
+    use WithPagination;
+
+    public function render()
+    {
+        return view('livewire.applications.review-queue', [
+            'applications' => StageApplication::where('status', 'submitted')
+                ->with(['student.user', 'company'])
+                ->latest('submitted_at')
+                ->paginate(15),
+        ]);
+    }
+}
